7db635a762b95881088890b503112358d0f8774b
Andrej Premrn <andrej.premrn@gmail.com>
2020-04-28 16:29:23 +0200

    Initial commit

---
+++ src/App/NormalizeStyles.js
@@ -0,0 +1,4 @@
+import normalize from 'normalize.css';
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`${normalize.toString()}`;
---
+++ src/App/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import BaseStyles from './BaseStyles';
+import NormalizeStyles from './NormalizeStyles';
+
+function App() {
+  return (
+    <Fragment>
+      <NormalizeStyles />
+      <BaseStyles />
+    </Fragment>
+  );
+}
+
+export default App;
